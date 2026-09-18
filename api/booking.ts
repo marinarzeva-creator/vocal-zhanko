401b898410918a5d7edfc792d0f36d5fed7d4058
marinarzeva-creator <marinarzeva@gmail.com>
2026-09-19 02:53:08 +0300

    Update booking.ts

--- api/booking.ts
+++ api/booking.ts
@@ -2,6 +2,10 @@
 import type { Request, Response } from "express";
 
 export default async function handler(req: Request, res: Response) {
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
   // Allow CORS if needed
   if (req.method === "OPTIONS") {
     return res.status(200).end();
